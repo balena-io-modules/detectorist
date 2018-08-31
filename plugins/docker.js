3894bf5681f651fd76280ff6c4a8a6b9db6363a3
John (Jack) Brown <jack@brownjohnf.com>
2018-08-31 13:10:34 -0700

    plugins/docker: Detect docker repos from .resinci.yml files
    Change-type: minor
    Signed-off-by: Jack Brown <jack@resin.io>

--- plugins/docker.js
+++ plugins/docker.js
@@ -1,18 +1,29 @@
 'use strict';
 
-const Promise  = require('bluebird')
-const { accessAsync, constants } = Promise.promisifyAll(require('fs'))
-const { join } = require('path')
+const _                                         = require('lodash')
+const Promise                                   = require('bluebird')
+const { accessAsync, constants, readFileAsync } = Promise.promisifyAll(require('fs'))
+const { join }                                  = require('path')
+const YAML                                      = require('yamljs')
 
 module.exports = {
   name: 'docker',
   test: path => {
     return accessAsync(join(path, 'Dockerfile'), constants.F_OK)
-    .then(() => true)
+    .then(() => {
+      // Dockerfile exists; this is definitely a docker repo
+      return true
+    })
     .catch(err => {
-      if (err.code === 'ENOENT') return false
+      if (err.code === 'ENOENT') return readFileAsync(join(path, '.resinci.yml'), 'utf8')
       console.error(err.message)
       throw err
+    })
+    .then(str => {
+      const config = YAML.parse(str)
+      if (_.get(config, 'docker.builds', []).length > 0) return true
+
+      return false
     })
   }
 }
