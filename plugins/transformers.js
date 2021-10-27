9e5ab09192f0da53770676e09aaed3b45200b50e
Stathis <emoraitidis@gmail.com>
2021-10-27 10:12:00 +0100

    Add transformers plugin
    Change-type: minor
    Signed-off-by: Stathis Moraitidis <stathis@balena.io>

---
+++ plugins/transformers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { join } = require('path');
+const { readYAML } = require('../lib/utils.js');
+
+const CONTRACT_PATH = 'balena.yml';
+
+module.exports = {
+	name: 'transformers',
+	test: (path) => {
+		return readYAML(join(path, CONTRACT_PATH))
+			.then((result) => {
+				// data.$transformer implies opt in to transformers flow
+				if (result.data.$transformer) {
+					return true;
+				}
+				return false;
+			})
+			.catch((err) => {
+				if (err.code === 'ENOENT') {
+					return false;
+				}
+				console.error(err.message);
+				throw err;
+			});
+	},
+};
